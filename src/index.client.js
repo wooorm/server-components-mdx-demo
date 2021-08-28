57237ee5e3830589b6ee436aba4ce96a34555631
Titus Wormer <tituswormer@gmail.com>
2021-08-28 10:47:16 +0200

    Update `react`

--- src/index.client.js
+++ src/index.client.js
@@ -1,4 +1,4 @@
-import {unstable_createRoot} from 'react-dom'
+import {hydrateRoot} from 'react-dom'
 import {createFromFetch} from 'react-server-dom-webpack'
 import {png2bin} from 'png2bin'
 import {Root} from './root.client.js'
@@ -9,9 +9,8 @@ async function main() {
   const $root = document.querySelector('#root')
   const $payload = document.querySelector('#payload')
   const url = URL.createObjectURL(new Blob([await png2bin($payload)]))
-  const root = unstable_createRoot($root, {hydrate: Boolean($root.innerHTML)})
 
-  root.render(<Root response={createFromFetch(fetch(url))} />)
+  hydrateRoot($root, <Root response={createFromFetch(fetch(url))} />)
 
   // Show that image.
   $payload.style = 'display:block'
